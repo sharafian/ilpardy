433f0cc79269e57b29c9f6044317696a593205dc
sharafian <ben@sharafian.me>
2017-07-04 21:56:48 +0200

    feat: pay with proper ILP

--- src/payer.js
+++ src/payer.js
@@ -1,5 +1,37 @@
+const ILP = require('ilp')
+const PluginBells = require('ilp-plugin-bells')
+
 module.exports = class Payer {
+  constructor (opts) {
+    this.spsp = opts.spsp
+
+    // TODO: get this properly once plugin bells works
+    const [ username, host ] = opts.spsp.split('@')
+    const account = 'https://' + host + '/ledger/accounts/' + username
+
+    this.plugin = new PluginBells({
+      account: account,
+      password: opts.pass     
+    })
+  }
+
+  getSPSP () {
+    return this.spsp
+  }
+
   async connect () {
-    return Promise.resolve()
+    await this.plugin.connect()
+  }
+
+  async pay (receiver, amount) {
+    const payment = await ILP.SPSP.quote(this.plugin, {
+      receiver,
+      sourceAmount: amount
+    })
+
+    payment.memo = 'ILPardy winner!'
+
+    await ILP.SPSP.sendPayment(this.plugin, payment)
+    console.log('sent', amount, 'from', this.spsp, 'to', receiver)
   }
 }
